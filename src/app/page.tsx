4e45efd0c4f4880909436bf90091346fb8ddab9a
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2024-08-06 18:10:52 +0200

    made beautiful heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+const mockPackages = [
+  "react",
+  "react-dom",
+  "react-router-dom",
+  "react-scripts",
+  "typescript",
+];
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (Patch Pulse In Progress)
+    <main className="p-4">
+      <div className="flex flex-wrap gap-4">
+        {mockPackages.map((pkg) => (
+          <div
+            key={pkg}
+            className="h-fit w-60 rounded-lg bg-slate-800 p-4 shadow"
+          >
+            <h3 className="text-xl font-semibold">{pkg}</h3>
+            <p className="text-sm text-slate-300">1.0.0</p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
