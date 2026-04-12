ea59ecf396c2f7dfa007c1e9b3356c5642567e21
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2026-04-12 13:21:53 +0200

    added gh stars count to docs header

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
       disable404Route: true,
       customCss: ['./src/styles/custom.css'],
       components: {
+        Header: './src/components/Header.astro',
         SiteTitle: './src/components/SiteTitle.astro',
         ThemeProvider: './src/components/DarkThemeProvider.astro',
         ThemeSelect: './src/components/ThemeTogglePlaceholder.astro',
